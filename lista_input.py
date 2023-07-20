0fc45097086a2b713f6b5757e0e0d7547dc723a8
Leogrimes25 <vleonardo203@gmail.com>
2023-07-20 20:34:20 -0300

    5

---
+++ lista_input.py
@@ -0,0 +1,4 @@
+lista= [int(x) for x in input().split()] # requsita ao terminal os valores inteiros da lista#
+for i in range(len(lista)):
+    print(" Dada a determinada lista o seu index é {} e o valor correspondente a este index é {}".format(i,lista[i]))
+    print('')
